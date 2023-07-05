cfe585f19c14ca4098f4498aba1c33227f3d9e55
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-05 22:08:45 +0200

    update angular pwa

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# angular_pwa
+# Angular PWA
+
+setup
+
+```bash
+ng new angular-pwa --routing true --style scss
+```
+
+add angular pwa
+
+```bash
+ng add @angular/pwa
+```
+
+```bash
+ng build
+```
+
+## Resource
+
+- https://angular.io/guide/service-worker-getting-started
+- https://ionicframework.com/docs/angular/pwa
+- https://hackernoon.com/building-progressive-web-application-pwa-with-angular
+- https://blog.briebug.com/blog/angular-progressive-web-application
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +21,7 @@ import { StyleguideComponent } from './pages/styleguide/styleguide.component';
 import { ColorsComponent } from './pages/styleguide/colors/colors.component';
 import { TestPipe } from './_test/test.pipe';
 import { TestComponent } from './_test/test/test.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,13 @@ import { TestComponent } from './_test/test/test.component';
 HttpClientModule,
 FormsModule,
 
-    AppRoutingModule
+    AppRoutingModule,
+     ServiceWorkerModule.register('ngsw-worker.js', {
+       enabled: !isDevMode(),
+       // Register the ServiceWorker as soon as the application is stable
+       // or after 30 seconds (whichever comes first).
+       registrationStrategy: 'registerWhenStable:30000'
+     })
   ],
   providers: [],
   bootstrap: [AppComponent]
