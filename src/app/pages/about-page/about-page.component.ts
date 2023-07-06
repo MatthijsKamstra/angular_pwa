2c82cbc0eca90bbd179cad0dc2a5d7d316423eea
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-06 09:58:11 +0200

    up

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -121,6 +121,32 @@ export class AboutPageComponent implements OnInit {
 			|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 	}
 
+
+	// getOS() {
+	// 	var userAgent = window.navigator.userAgent,
+	// 		platform = window.navigator?.userAgent?.platform || window.navigator.platform,
+	// 		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+	// 		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+	// 		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+	// 		os = null;
+
+	// 	if (macosPlatforms.indexOf(platform) !== -1) {
+	// 		os = 'Mac OS';
+	// 	} else if (iosPlatforms.indexOf(platform) !== -1) {
+	// 		os = 'iOS';
+	// 	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+	// 		os = 'Windows';
+	// 	} else if (/Android/.test(userAgent)) {
+	// 		os = 'Android';
+	// 	} else if (/Linux/.test(platform)) {
+	// 		os = 'Linux';
+	// 	}
+
+	// 	return os;
+	// }
+
+
+
 	getPlatformJS(): string {
 		return `${JSON.stringify(platform, null, 2)}`;
 	}
