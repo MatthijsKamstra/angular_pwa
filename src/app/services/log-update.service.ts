2530eb8d9ee5b89c2b40dca4fe9034fe349b1632
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-08 18:09:34 +0200

    see

--- src/app/services/log-update.service.ts
+++ src/app/services/log-update.service.ts
@@ -18,16 +18,20 @@ export class LogUpdateService {
 			switch (evt.type) {
 				case 'VERSION_DETECTED':
 					console.log(`Downloading new app version: ${evt.version.hash}`);
+					warn(`Downloading new app version: ${evt.version.hash}`);
 					this.notificationService.info(`Downloading new app version: ${evt.version.hash}`);
 					break;
 				case 'VERSION_READY':
 					console.log(`Current app version: ${evt.currentVersion.hash}`);
 					console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+					warn(`Current app version: ${evt.currentVersion.hash}`);
+					warn(`New app version ready for use: ${evt.latestVersion.hash}`);
 					this.notificationService.info(`Current app version: ${evt.currentVersion.hash}`);
 					this.notificationService.info(`New app version ready for use: ${evt.latestVersion.hash}`);
 					break;
 				case 'VERSION_INSTALLATION_FAILED':
 					console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+					warn(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
 					this.notificationService.info(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
 					break;
 			}
@@ -39,3 +43,7 @@ export class LogUpdateService {
 		this.notificationService.info(`HI`);
 	}
 }
+function warn(arg0: string) {
+	console.warn(arg0);
+}
+
