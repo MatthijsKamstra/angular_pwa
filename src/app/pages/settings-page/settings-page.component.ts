a042440dfc2106bec79bbed0c952dbb109fadbc6
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-08 20:44:06 +0200

    navigator data

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-settings-page',
-  templateUrl: './settings-page.component.html',
-  styleUrls: ['./settings-page.component.scss']
+	selector: 'app-settings-page',
+	templateUrl: './settings-page.component.html',
+	styleUrls: ['./settings-page.component.scss']
 })
-export class SettingsPageComponent {
+export class SettingsPageComponent implements OnInit {
+
+	ua: string = '';
+	platform: string = '';
+	userAgent: string = '';
+	_navigator: Navigator = navigator;
+
+	ngOnInit(): void {
+		this.ua = navigator.userAgent.toLowerCase();
+		var isAndroid = this.ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+
+
+		this.platform = navigator.platform;
+		this.userAgent = navigator.userAgent;
+
+
+		// console.log(navigator.userAgentData);
+
+	}
 
 }
