c699678a9e74a34baf2147689641232dd9d884c5
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-08 15:38:09 +0200

    update service

---
+++ src/app/services/handle-unrecoverable-state.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+// https://angular.io/guide/service-worker-communications#updating-to-the-latest-version
+
+@Injectable({
+	providedIn: 'root'
+})
+export class HandleUnrecoverableStateService {
+
+	constructor(updates: SwUpdate) {
+		updates.unrecoverable.subscribe(event => {
+			// notifyUser(
+			// 	'An error occurred that we cannot recover from:\n' +
+			// 	event.reason +
+			// 	'\n\nPlease reload the page.'
+			// );
+		});
+	}
+}
