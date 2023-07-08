775c05abcb43bf74ba14b49990c8e8c9a7736d8d
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-08 16:25:38 +0200

    tess

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { LogUpdateService } from 'src/app/services/log-update.service';
 import { NotificationService } from 'src/app/services/notification.service';
 
@@ -9,11 +10,22 @@ import { NotificationService } from 'src/app/services/notification.service';
 })
 export class DashboardPageComponent {
 
+	updateCheckText = '';
+
 	constructor(
 		private notificationService: NotificationService,
 		// private logUpdateService: LogUpdateService,
+		private update: SwUpdate
 	) {
 		// this.logUpdateService.hi();
+	}
+
+
+	updateCheck(): void {
+		this.update
+			.checkForUpdate()
+			.then(() => this.updateCheckText = 'resolved')
+			.catch(err => this.updateCheckText = `rejected: ${err.message}`);
 	}
 
 	onNotificationHandler() {
