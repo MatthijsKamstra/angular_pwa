86166a248c0167f41f4fc5d9ecb65086f961a66d
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-08 18:01:20 +0200

    another try

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -23,6 +23,7 @@ export class DashboardPageComponent {
 
 
 	onUpdate(): void {
+		this.notificationService.info('update');
 		this.updates
 			.checkForUpdate()
 			.then(() => this.updateCheckText = 'resolved')
@@ -35,7 +36,7 @@ export class DashboardPageComponent {
 
 
 	testHandler() {
-		this.notificationService.info('update');
+		this.notificationService.info('test');
 		this.onUpdate();
 
 		console.warn(this.updates.isEnabled);
@@ -46,19 +47,27 @@ export class DashboardPageComponent {
 			switch (evt.type) {
 				case 'VERSION_DETECTED':
 					console.log(`Downloading new app version: ${evt.version.hash}`);
+					consoleLog(`Downloading new app version: ${evt.version.hash}`);
 					this.notificationService.info(`Downloading new app version: ${evt.version.hash}`);
 					break;
 				case 'VERSION_READY':
 					console.log(`Current app version: ${evt.currentVersion.hash}`);
 					console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+					consoleLog(`Current app version: ${evt.currentVersion.hash}`);
+					consoleLog(`New app version ready for use: ${evt.latestVersion.hash}`);
 					this.notificationService.info(`Current app version: ${evt.currentVersion.hash}`);
 					this.notificationService.info(`New app version ready for use: ${evt.latestVersion.hash}`);
 					break;
 				case 'VERSION_INSTALLATION_FAILED':
 					console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+					consoleLog(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
 					this.notificationService.info(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
 					break;
 			}
 		});
 	}
+}
+
+function consoleLog(msg: string) {
+	console.log(msg);
 }
