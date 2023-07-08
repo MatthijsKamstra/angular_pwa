985a9bc2b3e5ffcca45f4ff5b44cc46ab4cc50d7
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-07-08 17:17:52 +0200

    tr

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -41,5 +41,24 @@ export class DashboardPageComponent {
 		console.warn(this.updates.isEnabled);
 		console.warn(this.updates.unrecoverable);
 		console.warn(this.updates.versionUpdates);
+
+		this.updates.versionUpdates.subscribe(evt => {
+			switch (evt.type) {
+				case 'VERSION_DETECTED':
+					console.log(`Downloading new app version: ${evt.version.hash}`);
+					this.notificationService.info(`Downloading new app version: ${evt.version.hash}`);
+					break;
+				case 'VERSION_READY':
+					console.log(`Current app version: ${evt.currentVersion.hash}`);
+					console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+					this.notificationService.info(`Current app version: ${evt.currentVersion.hash}`);
+					this.notificationService.info(`New app version ready for use: ${evt.latestVersion.hash}`);
+					break;
+				case 'VERSION_INSTALLATION_FAILED':
+					console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+					this.notificationService.info(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+					break;
+			}
+		});
 	}
 }
